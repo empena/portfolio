1d4c98046369276fb5677f9969209209edfa938d
Emily Pena <emilyapena@gmail.com>
2020-04-20 23:29:17 -0600

    projects controller

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,16 +1,42 @@
 class Api::ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :update, :destroy]
+
   def index
+    render json: Project.all
   end
 
   def show
+    render json: @project
   end
 
   def create
+    project = Project.new(project_params)
+
+    if project.save
+      render json: project
+    else
+      render json: project.errors, status: 422
+    end
   end
 
   def update
+    if @project.update(project_params)
+      render json: @project
+    else
+      render json: @project.errors, status: 422
+    end
   end
 
   def destroy
+    @project.destroy
   end
+
+  private
+    def set_project
+      @project = Project.find(params[:id])
+    end
+
+    def project_params
+      params.require(:project).permit(:name, :description, :category, :image, :link)
+    end
 end
