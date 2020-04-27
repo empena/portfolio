ce5aff81ebe70f0935b4cd5c5e835655830ab4d9
Emily Pena <emilyapena@gmail.com>
2020-04-27 00:00:49 -0600

    linked logo added styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,66 +1,74 @@
-import React from 'react';
-import '../styles/navbar.css'
+import React from "react";
+import "../styles/navbar.css";
+import { Link } from "react-router-dom";
 
-const Navbar = () => { 
+const Navbar = () => {
   return (
     <>
       <div style={styles.mainNav}>
         <div style={styles.leftNavStyles}>
+          <Link to="/">
             <h1 style={styles.logo}>EP</h1>
-          </div>
+          </Link>
+        </div>
         <div style={styles.rightNavStyles}>
-          <div className='main' id="portfolio" >
-            <a href="#portfolio" style={styles.button}>Portfolio</a>
+          <div className="main" id="portfolio">
+            <a href="#portfolio" style={styles.button}>
+              Portfolio
+            </a>
           </div>
-          <div className='main' id="contact" >
-            <a href="#contact" style={styles.button}>Contact</a>
+          <div className="main" id="contact">
+            <a href="#contact" style={styles.button}>
+              Contact
+            </a>
           </div>
-          <div className='main' id="findme" >
-            <a href="#findme" style={styles.button}>Find Me</a>
+          <div className="main" id="findme">
+            <a href="#findme" style={styles.button}>
+              Find Me
+            </a>
           </div>
         </div>
       </div>
-    </>    
-  )
-}
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
 
 const styles = {
   mainNav: {
-    display: 'flex',
-    height: '3em',
-    width: '100%',
-    top: '0',
-    backgroundColor: 'black',
-    position: 'fixed',
+    display: "flex",
+    height: "3em",
+    width: "100%",
+    top: "0",
+    backgroundColor: "#232323",
+    position: "fixed",
+    boxShadow: "0 2px 2px 0 rgba(0,0,0,0.24)",
   },
   rightNavStyles: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    width: '70%',
-    padding: '.8em .5em',
-    // border: '1px solid red',
+    display: "flex",
+    justifyContent: "flex-end",
+    width: "70%",
+    padding: ".8em .5em",
   },
   leftNavStyles: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '30%',
-    padding: '.5em .5em',
-    // border: '1px solid blue',
+    display: "flex",
+    justifyContent: "space-between",
+    width: "30%",
+    padding: ".5em .5em",
   },
   logo: {
-    color: '#00FFD1',
-    margin: '0em .5em',
-    fontSize: '23px',
-  },
-  button :{
-    padding: '1em 2em 1em',
-    fontSize: '12px',
-    color: '#ADADAD',
+    color: "#00FFD1",
+    margin: ".1em .5em",
+    fontSize: "23px",
     textDecoration: 'none',
-    textTransform: 'uppercase',
-    letterSpacing: '0.1em',
-    // border: '1px solid yellow',
-  }
-}
+  },
+  button: {
+    padding: "1em 2em 1em",
+    fontSize: "12px",
+    color: "#ADADAD",
+    textDecoration: "none",
+    textTransform: "uppercase",
+    letterSpacing: "0.1em",
+  },
+};
