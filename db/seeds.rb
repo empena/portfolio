3de6cb9d5167ef30ab7e39daf465e61709726838
Emily Pena <emilyapena@gmail.com>
2020-05-04 16:00:52 -0600

    added font awesome

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 project_list = [
-  [ "Aigwam", "Design", "Package Design by Emily Pena", "https://cdn.dribbble.com/users/2282250/screenshots/7081885/coffee-bag-packaging-design-.jpg", "https://github.com/devpointlabs/DigitalGloveBox" ],
-  [ "Wigwam", "Code", "Package Design by Emily Pena", "https://3.bp.blogspot.com/-Nvfm45xx00g/WvUgADDoQnI/AAAAAAAFF9g/OT0WBjlZFrYXNBxFk_fIn7hUTumkBTeSgCLcBGAs/s1600/Tra%2BDinh%2BTea%2B%25280%2529.jpg", "https://github.com/devpointlabs/DigitalGloveBox" ],
-  [ "Wigwam", "Code", "Package Design by Emily Pena", "https://3.bp.blogspot.com/-Nvfm45xx00g/WvUgADDoQnI/AAAAAAAFF9g/OT0WBjlZFrYXNBxFk_fIn7hUTumkBTeSgCLcBGAs/s1600/Tra%2BDinh%2BTea%2B%25280%2529.jpg", "https://github.com/devpointlabs/DigitalGloveBox" ],
+  [ "Aetna Federal Open Season", "Design", "Package Design by Emily Pena", "https://cdn.dribbble.com/users/2282250/screenshots/7081885/coffee-bag-packaging-design-.jpg", "https://github.com/devpointlabs/DigitalGloveBox" ],
+  [ "Grand Wailea", "Code", "Package Design by Emily Pena", "https://3.bp.blogspot.com/-Nvfm45xx00g/WvUgADDoQnI/AAAAAAAFF9g/OT0WBjlZFrYXNBxFk_fIn7hUTumkBTeSgCLcBGAs/s1600/Tra%2BDinh%2BTea%2B%25280%2529.jpg", "https://github.com/devpointlabs/DigitalGloveBox" ],
+  [ "Party Pop", "Code", "Package Design by Emily Pena", "https://3.bp.blogspot.com/-Nvfm45xx00g/WvUgADDoQnI/AAAAAAAFF9g/OT0WBjlZFrYXNBxFk_fIn7hUTumkBTeSgCLcBGAs/s1600/Tra%2BDinh%2BTea%2B%25280%2529.jpg", "https://github.com/devpointlabs/DigitalGloveBox" ],
   [ "Wigwam", "Design", "Package Design by Emily Pena", "https://cdn.dribbble.com/users/2282250/screenshots/7081885/coffee-bag-packaging-design-.jpg", "https://github.com/devpointlabs/DigitalGloveBox" ],
   [ "Wigwam", "Design", "Package Design by Emily Pena", "https://cdn.dribbble.com/users/2282250/screenshots/7081885/coffee-bag-packaging-design-.jpg", "https://github.com/devpointlabs/DigitalGloveBox" ],
   [ "Wigwam", "Code", "Package Design by Emily Pena", "https://3.bp.blogspot.com/-Nvfm45xx00g/WvUgADDoQnI/AAAAAAAFF9g/OT0WBjlZFrYXNBxFk_fIn7hUTumkBTeSgCLcBGAs/s1600/Tra%2BDinh%2BTea%2B%25280%2529.jpg", "https://github.com/devpointlabs/DigitalGloveBox" ],
