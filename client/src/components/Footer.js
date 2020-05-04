29227192038535c7460d47df7f923d8cb5e9c06b
Emily Pena <emilyapena@gmail.com>
2020-05-04 16:02:03 -0600

    changed to functional component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "../styles/ProjectCard.css";
+
+const Footer = () => {
+  return (
+    <div style={styles.footer}>
+      <p>&copy; EMILY PENA 2020</p>
+    </div>
+  );
+};
+
+export default Footer;
+
+const styles = {
+  footer: {
+    backgroundColor: "#232323",
+    height: "10em",
+    color: "#ADADAD",
+    fontSize: "10px",
+    textAlign: "center",
+    paddingTop: "10px",
+  },
+  p: {
+    color: "#ADADAD",
+    fontSize: "11px",
+  },
+  h1: {
+    color: "#00FFD1",
+  },
+  p2: {
+    color: "#ADADAD",
+    lineHeight: "23px",
+  },
+};
