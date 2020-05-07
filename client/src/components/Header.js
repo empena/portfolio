22564f01a05d362f48d80eb18fab13c9e93ee7e8
Emily Pena <emilyapena@gmail.com>
2020-05-07 17:33:45 -0600

    moved to component

---
+++ client/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div style={styles.header}>Coder / Designer</div>
+  )
+}
+
+export default Header
+
+const styles = {
+  header: {
+    backgroundColor: "#232323",
+    width: 'auto',
+    height: "auto",
+    color: "#00FFD1",
+    fontSize: "30px",
+    textAlign: "center",
+    padding: "100px",
+    marginTop: '1.5em',
+  },
+}
