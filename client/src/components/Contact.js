dfc4878a4709346473bc6f0fd921efea0ac571f8
Emily Pena <emilyapena@gmail.com>
2020-05-07 17:31:11 -0600

    responsive functionality

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,35 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ContactForm from "../forms/ContactForm";
-import "../styles/ProjectCard.css";
+import "../styles/Contact.css";
 import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Contact = () => {
   return (
     <section id="contact">
-      <div style={styles.contactContainerOne}>
-        <div style={styles.contactContainerTwo}>
-          <div style={styles.contact}>
-            <h1 style={styles.h1}>Contact</h1>
-            <p style={styles.p2}>
+      <div className="contactContainerOne">
+        <div className="contactContainerTwo">
+          <div className="contact">
+            <h1 className="h1">Contact</h1>
+            <p className="p2">
               I am interested in freelance opportunities. However if you have a
               request or question don’t hesitate to contact me with the form
               below.
             </p>
             <ContactForm />
           </div>
-          <div style={styles.findme}>
-            <h1 style={styles.h1}>Find Me</h1>
-            <div style={styles.iconContainer}>
-              <a href="https://github.com/empena" style={styles.circle}>
+          <div className="findme">
+            <h1 className="h1">Find Me</h1>
+            <div className="iconContainer">
+              <a href="https://github.com/empena" className="circle">
                 <FontAwesomeIcon icon={faGithub} size="lg" />
               </a>
               <a
                 href="https://www.linkedin.com/in/emilypena/"
-                style={styles.circle}
+                className="circle"
               >
                 <FontAwesomeIcon icon={faLinkedinIn} size="lg" />
               </a>
+              
+              {/* <a href="https://www.youracclaim.com/badges/b8076ad3-3d60-47b2-b439-3d60fbcb26e7/public_url">BADGE</a>
+              <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="b8076ad3-3d60-47b2-b439-3d60fbcb26e7" data-share-badge-host="https://www.youracclaim.com"></div><script type="text/javascript" async src="//cdn.youracclaim.com/assets/utilities/embed.js"></script> */}
+
             </div>
           </div>
         </div>
@@ -39,52 +43,3 @@ const Contact = () => {
 };
 
 export default Contact;
-
-const styles = {
-  contactContainerOne: {
-    backgroundColor: "#232323",
-    height: "auto",
-  },
-  contactContainerTwo: {
-    display: "flex",
-    maxWidth: "1024px",
-    margin: "auto",
-    paddingTop: "2em",
-    paddingBottom: "4em",
-  },
-  contact: {
-    display: "flex",
-    flexDirection: "column",
-    width: "60%",
-    paddingRight: "4em",
-  },
-  findme: {
-    display: "flex",
-    flexDirection: "column",
-    width: "40%",
-  },
-  iconContainer: {
-    display: "flex",
-    flexDirection: "row",
-  },
-  circle: {
-    marginRight: "1em",
-    border: "1px solid #00FFD1",
-    borderRadius: "50%",
-    width: "20px",
-    padding: ".75em",
-    textAlign: "center",
-    color: "#00FFD1",
-  },
-  p: {
-    color: "#ADADAD",
-    fontSize: "11px",
-  },
-  h1: {
-    color: "#00FFD1",
-  },
-  p2: {
-    color: "#ADADAD",
-    lineHeight: "23px",
-  },
-};
