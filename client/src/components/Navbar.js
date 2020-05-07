bcfe964527338d8be914eb975e37436740eff908
Emily Pena <emilyapena@gmail.com>
2020-05-07 15:36:15 -0600

    responsive navbar mq added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,74 +1,32 @@
 import React from "react";
 import "../styles/navbar.css";
-import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <>
-      <div style={styles.mainNav}>
-        <div style={styles.leftNavStyles}>
-          <Link to="/">
-            <h1 style={styles.logo}>EP</h1>
-          </Link>
+      <div className="mainNav">
+        <div className="leftNavStyles">
+          <a href="/" className="logo">
+            EP
+          </a>
         </div>
-        <div style={styles.rightNavStyles}>
-          <div className="main" id="portfolio">
-            <a href="#portfolio" style={styles.button}>
-              Portfolio
-            </a>
-          </div>
-          <div className="main" id="contact">
-            <a href="#contact" style={styles.button}>
-              Contact
-            </a>
-          </div>
-          <div className="main" id="findme">
-            <a href="#findme" style={styles.button}>
-              Find Me
-            </a>
-          </div>
+       <div className="rightNavStyles">
+          <a href="#portfolio" className="navbutton">
+            Portfolio
+          </a>
+          <a href="#contact" className="navbutton">
+            Contact
+          </a>
+          <a href="#contact" className="navbutton">
+            Find Me
+          </a>
         </div>
+        
       </div>
     </>
   );
 };
 
 export default Navbar;
-
-const styles = {
-  mainNav: {
-    display: "flex",
-    height: "3em",
-    width: "100%",
-    top: "0",
-    backgroundColor: "#232323",
-    position: "fixed",
-    boxShadow: "0 2px 2px 0 rgba(0,0,0,0.24)",
-  },
-  rightNavStyles: {
-    display: "flex",
-    justifyContent: "flex-end",
-    width: "70%",
-    padding: ".8em .5em",
-  },
-  leftNavStyles: {
-    display: "flex",
-    justifyContent: "space-between",
-    width: "30%",
-    padding: ".5em .5em",
-  },
-  logo: {
-    color: "#00FFD1",
-    margin: ".1em .5em",
-    fontSize: "23px",
-    textDecoration: 'none',
-  },
-  button: {
-    padding: "1em 2em 1em",
-    fontSize: "12px",
-    color: "#ADADAD",
-    textDecoration: "none",
-    textTransform: "uppercase",
-    letterSpacing: "0.1em",
-  },
-};
