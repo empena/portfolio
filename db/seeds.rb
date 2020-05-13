b79ca7de3587469046d0fd3fba1eb0f8ba62eaff
Emily Pena <emilyapena@gmail.com>
2020-05-12 22:02:44 -0600

    added data to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-project_list = [
-
+design_list = [
   [
     "Wigwam",
     "Design",
     "Package Design by Emily Pena",
     "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308642/Portfolio/Wigwam_thumb_xauszz.jpg",
-    "https://github.com/devpointlabs/DigitalGloveBox"
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308645/Portfolio/Wigwam1_pgsccz.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308646/Portfolio/Wigwam2_kbxrfm.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308646/Portfolio/Wigwam3_yoakhp.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308646/Portfolio/Wigwam4_nrcomi.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308645/Portfolio/Wigwam5_d461ly.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg"
   ],
 
   [
@@ -21,7 +17,12 @@ project_list = [
     "Design",
     "Menu Design by Emily Pena",
     "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308638/Portfolio/BattersUp_thumb_sddeix.jpg",
-    "https://github.com/devpointlabs/DigitalGloveBox"
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308642/Portfolio/BattersUp1_cmrx5k.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308642/Portfolio/BattersUp2_tlthiu.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
   ],
 
   [
@@ -29,20 +30,225 @@ project_list = [
     "Design",
     "Design by Emily Pena",
     "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308638/Portfolio/Benton_thumb_jj6cdm.jpg",
-    "https://github.com/devpointlabs/DigitalGloveBox"
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308640/Portfolio/Benton1_f5ppdj.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308641/Portfolio/Benton2_jmfzjf.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308641/Portfolio/Benton3_t3gqva.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308642/Portfolio/Benton4_d3djig.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
   ],
 
   [
     "4+1 Magazine",
     "Design",
-    "Design by Emily Pena",
+    "Magazine Design by Emily Pena",
     "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308637/Portfolio/4_1_thumb_gzygqh.jpg",
-    "https://github.com/devpointlabs/DigitalGloveBox"
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308638/Portfolio/4_1_1_wza5u0.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308639/Portfolio/4_1_2_pxhqqg.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308640/Portfolio/4_1_3_daup4a.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589308639/Portfolio/4_1_4_q8vrkk.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+  ],
+
+  [
+    "Ready to Pop",
+    "Design",
+    "Package Design by Emily Pena",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589337874/Portfolio/ReadyToPop_thumb_wdnvum.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589337874/Portfolio/ReadyToPop_1_fgxs6v.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589337875/Portfolio/ReadyToPop_2_jkhy8k.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589337876/Portfolio/ReadyToPop_3_t9lnkt.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+  ],
+
+  [
+    "Poster Collection",
+    "Design",
+    "Design by Emily Pena",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589337940/Portfolio/PosterCollection_thumb_ho0yku.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589337941/Portfolio/PosterCollection_1_e4zcac.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589337940/Portfolio/PosterCollection_2_vc2wr8.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589337940/Portfolio/PosterCollection_3_yfwr4f.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589337940/Portfolio/PosterCollection_4_ojdhjt.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589337940/Portfolio/PosterCollection_5_vpmomb.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589337939/Portfolio/PosterCollection_6_ykj7vi.jpg"
+  ],
+
+  [
+    "Party Pop",
+    "Design",
+    "Package Design by Emily Pena",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589338048/Portfolio/PartyPop_thumb_d1rgwv.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589338049/Portfolio/PartyPop_1_o37ji3.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589338048/Portfolio/PartyPop_2_ov3zzw.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+  ],
+
+  [
+    "Off the Hip",
+    "Design",
+    "Brand and Logo Design by Emily Pena",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589338079/Portfolio/OffTheHip_thumb_pu6kwf.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589338094/Portfolio/OffTheHip_1_jxalkd.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+  ],
+
+  [
+    "LWA Builders",
+    "Design",
+    "Brand and Logo Design by Emily Pena",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589338101/Portfolio/LWA_thumb_jv5aax.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589338102/Portfolio/LWA_1_mgg3rc.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+  ],
+
+  [
+    "Lily Bride Designs",
+    "Design",
+    "Logo Design by Emily Pena",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589338121/Portfolio/LilyBrideDesign_thumb_g6xuhb.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589338132/Portfolio/LilyBrideDesign1_rwwxex.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589338119/Portfolio/LilyBrideDesign2_qj3iv1.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+  ],
+
+  [
+    "Down The Aisle",
+    "Design",
+    "Package Design by Emily Pena",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340814/Portfolio/DownTheAisle_thumb_eihcbb.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340817/Portfolio/DownTheAisle_1_hanugk.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340819/Portfolio/DownTheAisle_2_lwm8ok.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+  ],
+
+  [
+    "Electra Type Specimen",
+    "Design",
+    "Design by Emily Pena",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340735/Portfolio/Electra_thumb_lefdtv.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340746/Portfolio/Electra_1_jt1qic.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340735/Portfolio/Electra_2_kygaob.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340735/Portfolio/Electra_3_xzfj17.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340735/Portfolio/Electra_4_aidrj3.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340735/Portfolio/Electra_5_fjkrgb.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+  ],
+
+  [
+    "Grand Wailea",
+    "Design",
+    "Reconnect Creative Concept, Creative Director: Jeff Olsen, Art Director: Emily Peña, Copywriter: Troy Thomas & Haley Larsen. 
+    Photo comps: Terry Vine Photography, Jason Lindsay Photography, Jim Ericson Photography, Colette De Barros Photography",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340624/Portfolio/GWConnections_thumb_jjua6z.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340625/Portfolio/GWConnections_1_zyoxju.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340624/Portfolio/GWConnections_2_yiiwju.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340625/Portfolio/GWConnections_3_gnowxq.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+  ],
+
+  [
+    "Grand Wailea",
+    "Design",
+    "5 Senses Creative Concept, Creative Director: Jeff Olsen, Art Director: Emily Peña, Copywriter:  Haley Larsen & Dan Von Der Embse. 
+    Photo comps: Terry Vine Photography, Jason Lindsay Photography, Jim Ericson Photography, Colette De Barros Photograph",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340596/Portfolio/GWFeels_thumb_cj3pwp.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340597/Portfolio/GWFeels_1_fhfc03.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340597/Portfolio/GWFeels_2_lcyefd.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340594/Portfolio/GWFeels_3_vlnhpr.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340595/Portfolio/GWFeels_4_jqrv1l.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340597/Portfolio/GWFeels_5_oetglg.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340597/Portfolio/GWFeels_6_cdrksg.png",
+  ],
+
+  [
+    "Grand Wailea",
+    "Design",
+    "Legacy Creative Concept, Creative Director: Jeff Olsen, Art Director: Emily Peña, Copywriter: Haley Larsen. 
+    Photo comps: Terry Vine Photography, Jason Lindsay Photography, Jim Ericson Photography, Colette De Barros Photograph",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340561/Portfolio/GWMemories_thumb_bzhkts.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340562/Portfolio/GWMemories1_bz0ler.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340562/Portfolio/GWMemories2_xa037w.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589340561/Portfolio/GWMemories3_h4b5vz.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589341865/Portfolio/filler_val4gr.jpg",
+  ],
+
+  [
+    "Aetna Federal Open Season",
+    "Design",
+    "Aetna Federal Open Season Campaign, Creative Director: Anna Levikova, Art Director: Emily Peña, Copywriter: Parker Dunn",
+  ]
+]
+
+design_list.each do | name, category, description, image, imagea, imageb, imagec, imaged, imagee, imagef |
+  Project.create( name: name, category: category, description: description,
+    image: image, imagea: imagea, imageb: imageb, imagec: imagec, imaged: imaged, imagee: imagee, imagef: imagef )
+end
+
+code_list = [
+  [
+    "LWA Builders",
+    "Code",
+    "Landing Page for Residential Conntractor",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589342289/Portfolio/Screen_Shot_2020-05-12_at_9.57.57_PM_p6ntna.png",
+    "Git code will go here",
+    "Live code will go here",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589342289/Portfolio/Screen_Shot_2020-05-12_at_9.57.57_PM_p6ntna.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589342289/Portfolio/Screen_Shot_2020-05-12_at_9.57.57_PM_p6ntna.png",
+  ],
+
+  [
+    "Digital GloveBox",
+    "Code",
+    "One stop shop for all of your vehicles information, a Digital Glove Box for you to access 
+    anytime, anywhere. When things go wrong, we have the information that matters most for your car.
+    Development team inlcudes Emily Pena, Qi Cao, Thomas Gaston, and Seth Wheeler",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589342289/Portfolio/Screen_Shot_2020-05-12_at_9.57.57_PM_p6ntna.png",
+    "https://github.com/devpointlabs/DigitalGloveBox",
+    "https://digital-glovebox.herokuapp.com",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589342289/Portfolio/Screen_Shot_2020-05-12_at_9.57.57_PM_p6ntna.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589342289/Portfolio/Screen_Shot_2020-05-12_at_9.57.57_PM_p6ntna.png",
+  ],
+
+  [
+    "Sacks and Such",
+    "Code",
+    "Application that manages business inventory selling through multiple 3rd party channels",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589342289/Portfolio/Screen_Shot_2020-05-12_at_9.57.57_PM_p6ntna.png",
+    "Git code will go here",
+    "Live code will go here",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589342289/Portfolio/Screen_Shot_2020-05-12_at_9.57.57_PM_p6ntna.png",
+    "https://res.cloudinary.com/dvwwzmsmn/image/upload/v1589342289/Portfolio/Screen_Shot_2020-05-12_at_9.57.57_PM_p6ntna.png",
   ],
 ]
 
-project_list.each do | name, category, description, image, link |
-  Project.create( name: name, category: category, description: description, image: image, link: link )
+code_list.each do | name, category, description, image, link, linka, imagea, imageb |
+  Project.create( name: name, category: category, description: description,
+    image: image, link: link, linka: linka, imagea: imagea, imageb: imageb )
 end
 
 puts "Seeded"
